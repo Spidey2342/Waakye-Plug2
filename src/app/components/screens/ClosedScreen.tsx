203095fff8bb22c4e4ca3f646b96196d366e3780
Spidey2342 <mygoals990@gmail.com>
2026-03-29 09:31:11 +0100

    kjhghj

--- src/app/components/screens/ClosedScreen.tsx
+++ src/app/components/screens/ClosedScreen.tsx
@@ -34,7 +34,7 @@ export function ClosedScreen({ timeUntilOpen }: ClosedScreenProps) {
           </div>
 
           <h1 className="text-3xl font-bold text-gray-900 mb-2">
-           Orders open at 5:30am 🌅
+           Orders open on Monday 🌅
           </h1>
 
           <p className="text-gray-600 mb-6">
