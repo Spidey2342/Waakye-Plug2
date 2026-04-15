109284fd6c116ba49625ee083d364a7ebb50fd0f
Spidey2342 <mygoals990@gmail.com>
2026-04-15 17:09:36 +0000

    asdf

--- src/app/utils/timeUtils.ts
+++ src/app/utils/timeUtils.ts
@@ -32,10 +32,10 @@ export function checkOrderingStatus(): OrderingStatus {
   const currentTime = currentHour * 60 + currentMinute;
 
   // 5:30 AM
-  const openTime = 5* 60 ;
+  const openTime = 15* 60 ;
 
   // 8:00 AM
-  const closeTime = 5* 60 + 10;
+  const closeTime = 22* 60 + 10;
       
   
   const isOpen = currentTime >= openTime && currentTime < closeTime;
@@ -46,14 +46,14 @@ export function checkOrderingStatus(): OrderingStatus {
   if (isOpen) {
 
     const closeDate = new Date(now);
-    closeDate.setHours(5, 10, 0, 0);
+    closeDate.setHours(22, 10, 0, 0);
 
     timeUntilClose = closeDate.getTime() - now.getTime();
 
   } else {
 
     const openDate = new Date(now);
-    openDate.setHours(5, 0, 0, 0);
+    openDate.setHours(15, 0, 0, 0);
 
     // If past today's closing time → open tomorrow
     if (currentTime >= closeTime) {
